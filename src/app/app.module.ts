62e8580bbaad3f78a27140ba6b41a3a6c100f4c8
Jacob Davis <jacob.marquez.davis@hotmail.com>
2020-05-21 16:40:07 -0400

    The goods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { SearchPokemonComponent } from './search-pokemon/search-pokemon.component';
 import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/search-pokemon/search-pokemon.component.ts
+++ src/app/search-pokemon/search-pokemon.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Pokemon } from '../pokemon';
+import { PokeapiService } from '../pokeapi-service.service';
 
 @Component({
   selector: 'app-search-pokemon',
@@ -8,11 +9,20 @@ import { Pokemon } from '../pokemon';
 })
 export class SearchPokemonComponent implements OnInit {
 
-  pokemon: Pokemon;
+  pokemon: Pokemon = {
+    name: "",
+    sprites: {
+      front_default: ""
+    }
+  };
 
-  constructor() { }
+  constructor(private pokeapiService: PokeapiService) { }
 
   ngOnInit(): void {
+  }
+
+  onClick(): void {
+    this.pokeapiService.getPokemonByName(this.pokemon.name).subscribe( res => this.pokemon = res);
   }
 
 }
